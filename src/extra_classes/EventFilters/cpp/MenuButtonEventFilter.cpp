1898f995e1062a9cbb4b488affd85fc82ba812da
Dominik <Dominik.wojtanowski@interia.pl>
2023-09-26 19:13:03 +0200

    Hovers work, cliks works :)

--- src/extra_classes/EventFilters/cpp/MenuButtonEventFilter.cpp
+++ src/extra_classes/EventFilters/cpp/MenuButtonEventFilter.cpp
@@ -16,7 +16,6 @@ bool MenuButtonEventFilter::eventFilter(QObject* obj, QEvent* event)
         QPoint pos = Event->pos();
         int x = pos.x();
         int y = pos.y();
-        std::cout << "Pozycja[" << x << ", " << y << "]" << std::endl;
         if(((x>=3 && x<=55 && y<=-3 && y>=-55) || (x == 0 && y == 0)))
         {
             if(wasChangedTop == false)
@@ -51,8 +50,13 @@ bool MenuButtonEventFilter::eventFilter(QObject* obj, QEvent* event)
     event->type() == QEvent::MouseButtonRelease ) {
         // Obsługuje kliknięcie myszą
             QMouseEvent* mouseEvent = static_cast<QMouseEvent*>(event);
-            if(mouseEvent->pos().x() > 317 )
+            std::cout << mouseEvent->pos().x() << ", " << mouseEvent->pos().y() << std::endl;
+            if(mouseEvent->pos().x() > 317 || (((mouseEvent->pos().x()>=3 && mouseEvent->pos().x()<=55 && 
+            mouseEvent->pos().y()<=-3 && mouseEvent->pos().y()>=-55) || (mouseEvent->pos().x() == 0 && mouseEvent->pos().y() == 0))))
+            {
+                wasChangedTop = false;
                 return false;
+            }    
             return true;
     }
     return false;
